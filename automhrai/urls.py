5f8de9a2a5739c9f0529025275fb92c0c5aee87e
VelankaniAIandCloudSolutions <aicloudsolutions@velankanigroup.com>
2024-02-15 17:29:03 +0530

    media fix

--- automhrai/urls.py
+++ automhrai/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
@@ -23,4 +26,4 @@ urlpatterns = [
     path('api/v1/candidate-ranking/', include('candidate_ranking.urls')),
     # path('api/v1/facial-recognition/', include('facial_recognition.urls')),
     path('api/v1/resume-parser/', include('resume_parser.urls')),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
