6f7a7d46579a4a5ca2f43e3a1ffe99942825065a
VelankaniAIandCloudSolutions <aicloudsolutions@velankanigroup.com>
2024-04-19 15:30:35 +0530

    changed website url

--- automhrai/settings.py
+++ automhrai/settings.py
@@ -183,7 +183,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
-WEBSITE_URL = 'http://localhost:8000'
+WEBSITE_URL = 'https://aidev.automhr.com'
 
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_RESULT_BACKEND = 'django-db'
