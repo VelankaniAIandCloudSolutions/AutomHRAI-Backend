87f105b7fdee0b5d518d0328fae16708a9ffd681
Ubuntu <ubuntu@ip-192-168-30-198.ap-south-1.compute.internal>
2024-05-13 09:22:46 +0000

    settings change

--- automhrai/settings.py
+++ automhrai/settings.py
@@ -128,22 +128,22 @@ WSGI_APPLICATION = 'automhrai.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'automhrai',
-#         'USER': 'automhrai_admin',
-#         'PASSWORD': 'Aic34062173',
-#         'HOST': 'automhrai.c6wmerccfdgz.ap-south-1.rds.amazonaws.com',   
-#         'PORT': '3306',       
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'automhrai',
+        'USER': 'automhrai_admin',
+        'PASSWORD': 'Aic34062173',
+        'HOST': 'automhrai.c6wmerccfdgz.ap-south-1.rds.amazonaws.com',   
+        'PORT': '3306',       
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
